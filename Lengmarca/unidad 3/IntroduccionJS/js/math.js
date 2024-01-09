9dd148fb56b4c4c9e06dbc3c260650459c3de6c8
Daniel OM <danicademic@outlook.com>
2024-01-09 18:23:00 +0000

    practicando variables lnd js

---
+++ Lengmarca/unidad 3/IntroduccionJS/js/math.js
@@ -0,0 +1,7 @@
+let numero6 = 15.435;
+console.log(Math.round(numero6));
+console.log(Math.ceil(numero6));
+console.log(Math.floor(numero6));
+console.log(Math.trunc(numero6));
+console.log(Math.abs(-234.643));
+console.log(Math.random());
---
+++ Lengmarca/unidad 3/IntroduccionJS/js/stringoperaciones.js
@@ -0,0 +1,6 @@
+const nombre = "Dani";
+const apellido = "Jorjaguer";
+
+console.log("Nombre: " + nombre + "\n" + "Apellido: " + apellido);
+
+console.log(`nombre ${nombre} apellido ${apellido}`);
