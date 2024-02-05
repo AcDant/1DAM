5d2282675fc6daf642714eab00a5b512622e710c
Daniel <danicademic@outlook.com>
2024-02-05 19:23:50 +0000

    lnd mensaje refactorizado

--- Lengmarca/2º trimestre/unidad 3/blogdecafe_fin/js/script.js
+++ Lengmarca/2º trimestre/unidad 3/blogdecafe_fin/js/script.js
@@ -47,19 +47,93 @@ const nombre = document.querySelector('#nombre');
 const email = document.querySelector('#email');
 const mensaje = document.querySelector('#mensaje');
 
-nombre.addEventListener('input', function(e){
-    console.log(e.target.value);
-})
-email.addEventListener('input', function(e){
-    console.log(e.target.value);
-})
-mensaje.addEventListener('input', function(e){
-    console.log(e.target.value);
-})
+
+// Eventos input
+// nombre.addEventListener('input', function(e){
+//     console.log(e.target.value);
+// })
+// email.addEventListener('input', function(e){
+//     console.log(e.target.value);
+// })
+// mensaje.addEventListener('input', function(e){
+//     console.log(e.target.value);
+// })
+
+// nombre.addEventListener('input', function(e){
+//     datos['nombre'] = e.target.value;
+//     console.log(datos);
+// })
+
+// email.addEventListener('input', function(e){
+//     datos['email'] = e.target.value;
+//     console.log(datos);
+// })
+// mensaje.addEventListener('input', function(e){
+//     datos['mensaje'] = e.target.value;
+//     console.log(datos);
+// })
+
+
+
+nombre.addEventListener('input',leerTexto)
+email.addEventListener('input',leerTexto)
+mensaje.addEventListener('input',leerTexto)
+
+function leerTexto (e){
+    datos[e.target.id] = e.target.value;
+    console.log(e)
+}
+
+
 
 formulario.addEventListener('submit',function(e){
     e.preventDefault();
+
+    // Comprobar que no haya campos de datos vacios
+
+    const {nombre, email, mensaje} = datos;
+
+    // Si un campo esta vacio, mostrar error
+if(nombre === "" || email === "" ||mensaje === ""){
+    // const hayError= true;
+    // mostrarError("Todos los campos son obligatorios");
+    mensajeEnviar("tODOS LOS CAMPOS SON OBLIGATORIOS", 'error')
+    return;
+}
+    // Si todo esta correcto enviar y mostrar mensaje de enviado
+    // const hayError= false;
+    // mostrarMensaje("Mensaje enviado correctamente")
+    mensajeEnviar("Mensaje enviado correctamente", 'ok')
 })
+
+// const mostrarMensaje = (mensaje) =>{
+//     const todoOk = document.createElement('p')
+//     todoOk.textContent=mensaje;
+//     todoOk.classList.add('ok');
+//     formulario.appendChild(todoOk);
+//     setTimeout(()=>{
+//         todoOk.remove()
+//     },2000)
+//     // console.log(mensaje)   
+// }
+
+// const mostrarError = (mensaje) =>{
+
+//     const alerta = document.createElement('p')
+//     alerta.textContent=mensaje;
+//     alerta.classList.add('error');
+//     formulario.appendChild(alerta);
+//     setTimeout(()=>{
+//         alerta.remove()
+//     },2000)
+//     // console.log(mensaje)
+// }
+
+
+const mensajeEnviar = (hayError) =>{
+    const mensaje = document.createElement('p')
+}
+
 
 
 
