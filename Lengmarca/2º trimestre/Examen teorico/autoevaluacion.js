90537c2e83cf48af812cccf4f6de690df963f637
Daniel <danicademic@outlook.com>
2024-03-05 18:21:43 +0000

    autoevaluacion

---
+++ Lengmarca/2º trimestre/Examen teorico/autoevaluacion.js
@@ -0,0 +1,18 @@
+
+// Tabla de multiplicar del 7
+console.log("Tabla multiplicar 7")
+for(i = 1; i<=10;i++){
+    console.log(7*i)
+}
+
+// Tabla de sumar del 8
+console.log("Tabla sumar 8")
+for(i=0;i<=8;i++){
+    console.log(8+i);
+}
+
+// Tabla de dividir del 9
+console.log("Tabla dividir 9")
+for(i=1;i<=9;i++){
+    console.log(9/i);
+}
