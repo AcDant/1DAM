87463757db9551e15bc469f0d0c6902bc4bb583a
Daniel_O <danicademic@outlook.com>
2024-03-09 17:22:04 +0000

    no funciona aprtado 2

--- Lengmarca/2º trimestre/Examen teórico/autoevaluacion.js
+++ Lengmarca/2º trimestre/Examen teórico/autoevaluacion.js
@@ -5,6 +5,15 @@ const multiplicar = document.querySelector(".section1");
 const sumar = document.querySelector(".section2");
 const dividir = document.querySelector(".section3");
 
+
+// Apartado 2
+
+const botonBin = document.getElementById("binario")
+const botonBin2 = document.getElementById("binario2") 
+
+
+// Fin apartado 2
+
 function multiplica(numero) {
   console.log("Tabla multiplicar " + numero);
   for (i = 1; i <= 10; i++) {
@@ -68,5 +77,36 @@ function operar() {
 
 
 // Apartado 2
+// let pvalorBin = "77"
+// let valorBinNumero;
+// let valorOnBin;
+// valorOnBin = Number(pvalorBin).toString(2);
+// console.log(valorOnBin)
 
- 
+// const botonBin = document.getElementById("binario3")
+// const botonBin2 = document.getElementById("binario2") 
+// const botonBin4 = document.getElementById("binario4") 
+console.log(botonBin);
+console.log(botonBin2);
+console.log(botonBin4);
+console.log(numero);
+let valorBin = document.querySelector("#num-bin"); 
+
+
+if(botonBin != null){
+  // Adding a click event script to null object
+  botonBin.addEventListener('click', calcula)
+}else{
+  console.warn(`#SampleEl Object or Element Does not exists or not yet loaded.`)
+}
+
+// botonBin.addEventListener("click", calcula);
+
+
+function calcula(){
+  console.log(valorOnBin)
+  valorBin = document.querySelector("#num-bin").value;
+  let valorBinNumero;
+  let valorOnBin;
+  valorOnBin = Number(valorBin).toString(2);
+}
