c6e55d55ac4f9f3293ba6b701c0fe45d1b0802e1
Daniel_O <danicademic@outlook.com>
2024-05-27 22:39:35 +0100

    bae

--- Bases de datos/3º trimestre/Proyecto individual/Inserciones_biblioteca.sql
+++ Bases de datos/3º trimestre/Proyecto individual/Inserciones_biblioteca.sql
@@ -1,8 +1,16 @@
+-- inserciones en editorial
 insert into editoriales values (default,'jazmin',
 	row('Calle keke',3,'Telde', 'Telde'),'{123123123,321321321}','www.jazmin.com');
 insert into editoriales values (default,'jazmin',
 	row('Calle puentin',2,'Las Remudas', 'Telde'),'{987987987,789789789}','www.jazmin.com');
 insert into editoriales values (default,'jazmin',
 	row('Calle sarajevo',1,'Arucas', 'Arucas'),'{123123123,321321321}','www.jazmin.com');
-insert into editoriales values (default,'jazmin',
-	row('Calle rumbiente',5,'Telde', 'Telde'),'{123123123,321321321}','www.jazmin.com');
+
+-- inserciones libros
+  isbn VARCHAR(50) PRIMARY KEY,
+  titulo VARCHAR(255) NOT NULL,
+  genero VARCHAR(20) NOT NULL,
+  fecha_publicacion DATE NOT NULL,
+  idioma VARCHAR(30) NOT NULL,
+  numero_paginas INTEGER NOT NULL,
+  editorial integer NOT NULL
