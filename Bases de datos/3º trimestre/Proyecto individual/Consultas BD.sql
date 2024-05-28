48c0eef1bac27036e2f288998822e5ad92331038
Daniel <danicademic@outlook.com>
2024-05-28 17:48:18 +0100

    consultas

--- Bases de datos/3º trimestre/Proyecto individual/Consultas BD.sql
+++ Bases de datos/3º trimestre/Proyecto individual/Consultas BD.sql
@@ -1,9 +1,10 @@
 -- 1º muestra los libros de x autor (subconsulta)
-
+select * from libros where isbn = (select libro_isbn from creaciones where id_autor  = 
+	(select id_autor from autores where nombre = 'Myke'));
 -- 2º muestra los libros de x editorial (subconsulta)
-
+select * from libros where editorial in (select id_editorial from editoriales where nombre = 'jazmin');
 -- 3º muestra el usuario y libro de x prestamo (join)
-
+select * from 
 -- 4º muestra editorial y autor de x libro (join)
 
 -- 5º muestra las editoriales que tengan libro en prestamo
