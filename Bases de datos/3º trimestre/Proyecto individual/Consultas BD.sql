5d820a4d5283a4c0de7eb9be46acad0a31cfbbb5
Daniel <danicademic@outlook.com>
2024-05-28 17:24:34 +0100

    consuktas

--- Bases de datos/3º trimestre/Proyecto individual/Consultas BD.sql
+++ Bases de datos/3º trimestre/Proyecto individual/Consultas BD.sql
@@ -1,5 +1,11 @@
 -- 1º muestra los libros de x autor (subconsulta)
 
--- 2º muestra los libros los libros de x editorial (subconsulta)
+-- 2º muestra los libros de x editorial (subconsulta)
 
--- 3º 
+-- 3º muestra el usuario y libro de x prestamo (join)
+
+-- 4º muestra editorial y autor de x libro (join)
+
+-- 5º muestra las editoriales que tengan libro en prestamo
+
+-- 6º muestra usuarios que tengan libro en prestamo
