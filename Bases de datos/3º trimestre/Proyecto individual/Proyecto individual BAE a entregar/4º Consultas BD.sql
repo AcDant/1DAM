e923637aad4e151f9b99be010d279973bc7834dc
Daniel_O <danicademic@outlook.com>
2024-06-02 20:00:04 +0100

    consultas proyect obae modificacion pequeña

--- Bases de datos/3º trimestre/Proyecto individual/Proyecto individual BAE a entregar/4º Consultas BD.sql
+++ Bases de datos/3º trimestre/Proyecto individual/Proyecto individual BAE a entregar/4º Consultas BD.sql
@@ -1,11 +1,11 @@
 -- 1º muestra los libros de x autor (subconsulta)
-select * from libros where isbn = (select libro_isbn from creaciones where id_autor  = 
+select * from libros where isbn = (select libro_isbn from creaciones where id_autor  in 
 	(select id_autor from autores where nombre = 'Myke'));
 -- 2º muestra los libros de x editorial (subconsulta)
 select * from libros where editorial in (select id_editorial from editoriales where nombre = 'jazmin');
 -- 3º muestra el usuario y libro con prestamo de fecha x (join)
 select usuarios.nombre, libros.titulo from usuarios join prestamos on usuarios.id_usuario = prestamos.id_usuario 
-	join libros on prestamos.isbn = libros.isbn where prestamos.fecha_prestamo = '2024-05-23';
+	join libros on prestamos.isbn = libros.isbn where prestamos.fecha_prestamo = '2024-05-29';
 -- 4º muestra editorial y autor de x libro (join)
 select editoriales.nombre, autores.nombre from editoriales join libros on editoriales.id_editorial = libros.editorial 
 	join creaciones on libros.isbn = creaciones.libro_isbn join autores on autores.id_autor = creaciones.id_autor 
